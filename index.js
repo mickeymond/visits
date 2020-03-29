ffca6fc058137e5ea30a516a8a324e26e6337a8d
Michael Hammond <mickeymond@gmail.com>
2020-03-29 15:05:23 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const redis = require('redis');
+
+const app = express();
+const client = redis.createClient({
+  host: 'redis-server'
+});
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+  client.get('visits', (err, visits) => {
+    if (!err) {
+      res.send(`Number of visits is ${visits}`);
+      client.set('visits', parseInt(visits) + 1);
+      console.log('Updated the Redis Cache');
+    }
+  })
+});
+
+app.listen(8081, () => {
+  console.log('Listening on PORT 8081');
+});
